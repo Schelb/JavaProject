ab9486c9371ca499c3c83b081802d6353c89e5d3
Sсhelb <bokovikov.mikhail@gmail.com>
2021-10-06 22:43:31 +0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        CreateRecord createRecord= new CreateRecord();
+        createRecord.fillRecord();
+    }
+}
+
+
+
---
+++ src/main/java/org/example/CreateRecord.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.ArrayList;
+
+enum Type{
+    Relevant,
+    Irrelevant,
+    RelevantNotFound
+}
+public class CreateRecord {
+    public static void fillRecord() {
+        int n = 10;
+        int m = 15;
+        ArrayList<Type> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            double j = Math.random();
+            if (j > 0.5) {
+                result.add(Type.Relevant);
+            } else {
+                result.add(Type.Irrelevant);
+            }
+        }
+        for (int i = n; i < m; i++) {
+            result.add(Type.RelevantNotFound);
+        }
+        for (int i = 0; i < m; i++) {
+            System.out.println(i + 1 + " " + result.get(i));
+        }
+    }
+}
