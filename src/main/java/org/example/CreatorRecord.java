b3a5578f8f529a275a0a0777dca73b84ceb2a874
Sсhelb <bokovikov.mikhail@gmail.com>
2021-10-07 11:31:50 +0400

    Add enum RecordType, implemented the functionality of the CreatorRecord class

--- src/main/java/org/example/CreatorRecord.java
+++ src/main/java/org/example/CreatorRecord.java
@@ -1,4 +1,45 @@
 package org.example;
 
 public class CreatorRecord {
+    public Record createRelevantRecord() {
+        /*       int n = 10;
+        int m = 15;
+        ArrayList<Record.Type> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            double j = Math.random();
+            if (j > 0.5) {
+                result.add(Record.Type.Relevant);
+            } else {
+                result.add(Record.Type.Irrelevant);
+            }
+        }
+        for (int i = n; i < m; i++) {
+            result.add(Record.Type.RelevantNotFound);
+        }
+        for (int i = 0; i < m; i++) {
+            System.out.println(i + 1 + " " + result.get(i));
+
+        double j = Math.random();
+        if (j > 0.5) {
+            record.setType(Type.Relevant);
+        } else {
+            record.setType(Type.Irrelevant);
+        }
+        return record;
+        } */
+        Record recordRelevant=  new Record();
+        recordRelevant.setType(RecordType.Relevant);
+        return recordRelevant;
+
+    }
+    public Record createIrrelevantRecord(){
+        Record recordIrrelevant=  new Record();
+        recordIrrelevant.setType(RecordType.Irrelevant);
+        return recordIrrelevant;
+    }
+    public Record createRelevantNotFoundRecord(){
+        Record recordRelevantNotFoundRecord=  new Record();
+        recordRelevantNotFoundRecord.setType(RecordType.RelevantNotFound);
+        return recordRelevantNotFoundRecord;
+    }
 }
--- src/main/java/org/example/Record.java
+++ src/main/java/org/example/Record.java
@@ -2,30 +2,12 @@ package org.example;
 
 import java.util.ArrayList;
 
-
 public class Record {
-    enum Type{
-        Relevant,
-        Irrelevant,
-        RelevantNotFound
+    private RecordType type;
+    public RecordType  getType(){
+        return this.type;
     }
-    public static void fillRecord() {
-        int n = 10;
-        int m = 15;
-        ArrayList<Type> result = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            double j = Math.random();
-            if (j > 0.5) {
-                result.add(Type.Relevant);
-            } else {
-                result.add(Type.Irrelevant);
-            }
-        }
-        for (int i = n; i < m; i++) {
-            result.add(Type.RelevantNotFound);
-        }
-        for (int i = 0; i < m; i++) {
-            System.out.println(i + 1 + " " + result.get(i));
-        }
+    public void setType(RecordType typeIn){
+        this.type= typeIn;
     }
 }
