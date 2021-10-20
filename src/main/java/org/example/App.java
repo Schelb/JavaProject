7640452d2a4d7b689a2ea992e69210f8e76fbec1
Sсhelb <bokovikov.mikhail@gmail.com>
2021-10-20 21:48:16 +0400

    Add class Controller

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -26,6 +28,24 @@ public class App extends Application
         stage.setWidth(1500);
         stage.setHeight(500);
         stage.show();
+
+        CreatorRecord creatorRecord=new CreatorRecord();
+        int n = 10;
+        int m = 15;
+        ArrayList<Record> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            double j = Math.random();
+            if (j > 0.5) {
+                result.add(creatorRecord.createRelevantRecord());
+            } else {
+                result.add(creatorRecord.createIrrelevantRecord());
+            }
+        }
+        for (int i = n; i < m; i++) {
+            result.add(creatorRecord.createRelevantNotFoundRecord());
+        }
+        ListOfRecords records= new ListOfRecords(result);
+
     }
 }
 
--- src/main/java/org/example/ListOfRecords.java
+++ src/main/java/org/example/ListOfRecords.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListOfRecords {
-    public List<Record> records;
-    public void ListOfRecords(){
+    public  List<Record> records;
+    public ListOfRecords(){
         records = new ArrayList<>();
+    }
+    public ListOfRecords(ArrayList<Record> rds){
+        records = new ArrayList<>();
+        records=rds;
     }
 }
